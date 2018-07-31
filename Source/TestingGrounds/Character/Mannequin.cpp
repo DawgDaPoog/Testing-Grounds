c16cdb1ee461c5f5cdb40d06752d946f88058578
Vlad <Kulini4vlad@ukr.net>
2018-07-31 12:59:18 +0300

    Improved Gun Logic. Improved AI Gun handling and logic

--- Source/TestingGrounds/Character/Mannequin.cpp
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -44,6 +44,10 @@ void AMannequin::BeginPlay()
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	if (InputComponent != NULL) {
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
 }
 
 // Called every frame
@@ -57,10 +61,10 @@ void AMannequin::Tick(float DeltaTime)
 void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	
 }
 
-void AMannequin::Fire()
+void AMannequin::PullTrigger()
 {
 	Gun->OnFire();
 }
